de5bb08a785b7b317d5fabcc909a6acb73c8a312
DmitryNikolaev <dmi3y_tsk@bk.ru>
2023-02-07 11:04:38 +0200

    8

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/test-task-tl/'
+  ? '/test-task-tl/dist/'
   : '/'
 })
