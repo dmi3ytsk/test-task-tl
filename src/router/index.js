f78a084aa1427a4df2893339f31fb12759774fc4
DmitryNikolaev <dmi3y_tsk@bk.ru>
2023-02-07 10:47:47 +0200

    4

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,11 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-  {
-    path: "/test-task-tl/",
-    name: "home",
-    component: HomeView,
-  },
+  // {
+  //   path: "/test-task-tl/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
   {
     path: "index.html",
     name: "home-gh",
